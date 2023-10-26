f6c71f43f39e88cf0a986c3541fc9c3881f0914f
RayaneB-GIT <ray.lou.com@gmail.com>
2023-10-26 14:50:47 -0400

    function button scroolUp worked, image tower adding, footer adding, some css adding on it

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
-  }
-  
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  } 
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+/*The scrollTop method is already existing, it is why it had not worked, but you can right a new method called scrolltop */
+function scrolltop() {
+  window.scrollTo(0, 0);
+};
+
