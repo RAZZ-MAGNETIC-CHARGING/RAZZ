b53c68e239b71fd92656c17b134da9e24ac807f1
Rayhan Karim <akari033@uottawa.ca>
2023-10-27 12:10:20 -0400

    rounded the website to make it more modern, polished the drop down menu

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+let menuShow = false
+
+function openClose() {
+  if (menuShow) {
+    closeNav()
+    menuShow = false
+  } else {
+    openNav()
+    menuShow = true
+  }
+}
+
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
+  document.getElementById("mySidepanel").style.height = "250px";
+  document.getElementById("mySidepanel").style.paddingTop = "60px";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
+  document.getElementById("mySidepanel").style.height = "0px";
+  document.getElementById("mySidepanel").style.paddingTop = "0px";
 }
 
 /*The scrollTop method is already existing, it is why it had not worked, but you can right a new method called scrolltop */
